cb4602910095ed72cd80aee7fd76aa2166aa2972
ilfana <>
2026-06-22 15:28:49 +0500

    feat/fix/refactor(keymap.lua): Replace copy-paste keybindings to loop. Add new keybindings. Disabled arrows

--- user/.config/nvim/lua/config/keymap.lua
+++ user/.config/nvim/lua/config/keymap.lua
@@ -1,12 +1,37 @@
-vim.keymap.set({"i", "n"}, "<C-e>", ":NvimTreeToggle<CR>")
-vim.keymap.set({"i", "n"}, "<C-j>", ":ToggleTerm<CR>")
+-- Vanilla
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>x<CR>")
 
-vim.keymap.set("n", "<A-1>", ":BufferLineGoToBuffer 1<CR>")
-vim.keymap.set("n", "<A-2>", ":BufferLineGoToBuffer 2<CR>")
-vim.keymap.set("n", "<A-3>", ":BufferLineGoToBuffer 3<CR>")
-vim.keymap.set("n", "<A-4>", ":BufferLineGoToBuffer 4<CR>")
-vim.keymap.set("n", "<A-5>", ":BufferLineGoToBuffer 5<CR>")
-vim.keymap.set("n", "<A-6>", ":BufferLineGoToBuffer 6<CR>")
-vim.keymap.set("n", "<A-7>", ":BufferLineGoToBuffer 7<CR>")
-vim.keymap.set("n", "<A-8>", ":BufferLineGoToBuffer 8<CR>")
-vim.keymap.set("n", "<A-9>", ":BufferLineGoToBuffer 9<CR>")
+
+vim.keymap.set("n", "<C-h>", ":%s/")
+vim.keymap.set("n", "<leader>cl", "<cmd>noh<CR>")
+
+-- Nvim Tree
+vim.keymap.set({"i", "n"}, "<C-e>", "<cmd>NvimTreeToggle<CR>")
+
+
+-- Toggle Term
+vim.keymap.set({"i", "n"}, "<C-j>", "<cmd>ToggleTerm<CR>")
+
+
+-- Buffer Line
+for i = 1, 9 do
+    vim.keymap.set({"n", "i"},
+    "<A-" .. i .. ">",
+    "<cmd>BufferLineGoToBuffer " .. i .. "<CR>")
+end
+
+vim.keymap.set({"n", "i"}, "<A-h>", "<cmd>BufferLineCyclePrev<CR>")
+vim.keymap.set({"n", "i"}, "<A-l>", "<cmd>BufferLineCycleNext<CR>")
+
+
+-- Unbind Arrows
+for _, arrow in ipairs({"Up", "Down", "Left", "Right"}) do
+    vim.keymap.set({"n", "i", "v"}, "<" .. arrow .. ">", "<Nop>")
+end
+
+-- Diagnostics
+vim.keymap.set({"n", "i"}, "<C-d>", vim.diagnostic.open_float)
+
+-- LSP
+vim.keymap.set({"i", "n"}, "<C-l>",vim.lsp.buf.hover)
